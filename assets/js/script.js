caaf24442fb3bcf7aa9669681a9f6fdeea2c6292
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-01-09 13:46:40 -0600

    outline of redo for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//variables for times
+
+//task storage function
+
+//date and time from moment and to change color based off task time
+
+//function for saving tasks
+
+//function to update calendar
